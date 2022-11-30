4c8f20ab1cb65bbc82be664c4c5392e57146ad62
ilieBeracha <ilieberacha123@gmail.com>
2022-11-30 17:05:32 +0200

    icons

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,31 +1,70 @@
+import { Avatar } from "@mui/material";
 import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 import { scrollTo } from "../../functions/scrollTo";
+import HomeIcon from '@mui/icons-material/Home';
+import MovieIcon from '@mui/icons-material/Movie';
+import TvIcon from '@mui/icons-material/Tv';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import AcUnitSharpIcon from '@mui/icons-material/AcUnitSharp';
 import "./Header.css";
+import { toast } from "react-toastify";
 
 function Header(): JSX.Element {
     const favoriteSelector = useSelector((state: any) => state.favorite.values);
 
+    function toastMess(){
+        toast.info('Coming soon...',{
+            position: toast.POSITION.TOP_CENTER,
+            className: 'discoverToast',
+            theme: "colored",
+            // hideProgressBar:true,
+            closeOnClick:true,
+            draggable:true,
+            pauseOnHover:false,
+        })
+    }
 
     return (
         <div className="Header">
             <NavLink to={'/'}>
                 <div onClick={() => scrollTo.scrollTo()} className="Header_Headings">
+                    {/* <AcUnitSharpIcon fontSize="large"/> */}
                     <h1>Need's</h1>
                 </div>
             </NavLink>
 
             <div className="Header_NavBar">
-                <NavLink to={'/'}>Home</NavLink>
-                <NavLink to={'/movies'}>Movies</NavLink>
-                <NavLink to={'/tv'}>Tv Shows</NavLink>
-                {favoriteSelector.length!==0?
-                    <NavLink to={'/fav'}>Favorite</NavLink>
-                :<></>}
+                <div className="Header_NavBar_div">
+                    <NavLink to={'/'}>
+                        <HomeIcon fontSize="medium" />
+                        <span>Home</span>
+                    </NavLink>
+                </div>
+                <div className="Header_NavBar_div">
+                    <NavLink to={'/movies'}>
+                        <MovieIcon fontSize="medium" />
+                        <span>Movies</span>
+                    </NavLink>
+                </div>
+                <div className="Header_NavBar_div">
+                    <NavLink to={'/tv'}>
+                        <TvIcon fontSize="medium" />
+                        <span>Tv Shows</span>
+                    </NavLink>
+                </div>
+                {favoriteSelector.length !== 0 ?
+                    <div className="Header_NavBar_div">
+                        <NavLink to={'/fav'}>
+                            <FavoriteIcon fontSize="medium" />
+                            <span>Favorite</span>
+                        </NavLink>
+                    </div>
+                : <></>}
             </div>
-
             <div className="Header_Log">
-                <img src='' alt="" />
+                <Avatar className="AvatarLogo" onClick={toastMess}>I</Avatar>
+
             </div>
         </div>
     );
